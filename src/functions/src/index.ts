c00d63e8f28ab0f7ab70a9df88ce60a3e5206a7b
Fabien Lavocat <fabien.lavocat@dolby.com>
2023-06-01 15:31:00 -0700

    Update dependencies

--- src/functions/src/index.ts
+++ src/functions/src/index.ts
@@ -1,10 +1,13 @@
-import {https} from "firebase-functions";
-import {communications} from "@dolbyio/dolbyio-rest-apis-client";
+import { https } from "firebase-functions";
+import { communications } from "@dolbyio/dolbyio-rest-apis-client";
 
 // Enter your App Key and Secret from the dolby.io dashboard
 const APP_KEY = "APP_KEY";
 const APP_SECRET = "APP_SECRET";
 
 export const getAccessToken = https.onCall((_data, _context) => {
-  return communications.authentication.getClientAccessToken(APP_KEY, APP_SECRET);
+  return communications.authentication.getClientAccessToken(
+    APP_KEY,
+    APP_SECRET
+  );
 });
